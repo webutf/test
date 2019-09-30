3853db3e88caf92effdb15ac7e4763e0365c3406
webnian <919882905@qq.com>
2019-09-30 21:10:43 +0800

    ceshi

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 console.log(456465)
 self.addEventListener('fetch', function(e) {
 	console.log(fetch)
@@ -6,3 +7,33 @@ self.addEventListener('fetch', function(e) {
 self.addEventListener('install', function(e) {
 	console.log('install')
 })
+
+if (!workbox) {
+	return console.log(456465465)
+}
+
+workbox.routing.registerRoute(
+	/\.min\.js$/,
+	new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+	/\.css$/,
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: 'css-cache',
+	})
+);
+
+workbox.routing.registerRoute(
+	/\.(?:png|jpg|jpeg|svg|gif)$/,
+	// Use the cache if it's available.
+	new workbox.strategies.CacheFirst({
+		cacheName: 'image-cache',
+		plugins: [
+			new workbox.expiration.Plugin({
+				maxEntries: 20,
+				maxAgeSeconds: 7 * 24 * 60 * 60,
+			})
+		],
+	})
+);
