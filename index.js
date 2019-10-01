8666cdb538cfaf9d5c76b05f8d2dc7fd476f4bd3
webnian <919882905@qq.com>
2019-10-01 08:47:48 +0800

    ceshi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./mods/sw.js').then(res => {
+	navigator.serviceWorker.register('./mods/sw.js', {scope: '/'}).then(res => {
 		console.log(res)
 	}).catch(error => {
 
