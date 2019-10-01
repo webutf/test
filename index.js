3442f6f2a2a1f340fe9b2da6cb66eebb2c43c292
webnian <919882905@qq.com>
2019-10-01 08:53:25 +0800

    ceshi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./mods/sw.js', {scope: '/'}).then(res => {
+	navigator.serviceWorker.register('./mods/sw.js').then(res => {
 		console.log(res)
 	}).catch(error => {
 
---
+++ mods/common/sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('fetch', function(e) {
+	console.log(e)
+})
+console.log(4564654646)
+
